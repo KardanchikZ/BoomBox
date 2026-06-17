b5695b1c41f1e6626a3f7f2026dca11c61934516
timuuur <isroil75timur@gmail.com>
2026-06-17 17:10:09 +0500

    new plugin

--- Items/BoomBox.cs
+++ Items/BoomBox.cs
@@ -1,11 +1,11 @@
-﻿using Exiled.API.Features.Attributes;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Pickups;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Exiled.Events.EventArgs.Map;
+using Exiled.Events.EventArgs.Player;
+using UnityEngine;
 
 namespace BoomBox.Items
 {
@@ -14,8 +14,58 @@ namespace BoomBox.Items
     {
         public override uint Id { get; set; } = Config.IdBoomBox;
         public override string Name { get; set; } = "BoomBox";
-        public override string Description { get; set; } = "PlayMusic";
+        public override string Description { get; set; } = "Play Music";
         public override float Weight { get; set; } = 1f;
-        public override SpawnProperties SpawnProperties { get; set; }
+        public override SpawnProperties SpawnProperties { get; set; } = new()
+        {
+            RoomSpawnPoints = new()
+            {
+                new RoomSpawnPoint
+                {
+                    Chance = 100,
+                    Room = RoomType.HczNuke,
+                    Offset = new Vector3(0, 0, 0)
+                }
+            }
+        };
+
+        int MusicId = 0;
+
+        protected override void SubscribeEvents()
+        {
+            Exiled.Events.Handlers.Player.TogglingRadio += OnTogglingRadio;
+            Exiled.Events.Handlers.Map.PickupAdded += PickupAdded;
+            Exiled.Events.Handlers.Player.ChangingRadioPreset += ChangingRadioPreset;
+            base.SubscribeEvents();
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            Exiled.Events.Handlers.Player.TogglingRadio -= OnTogglingRadio;
+            Exiled.Events.Handlers.Map.PickupAdded -= PickupAdded;
+            Exiled.Events.Handlers.Player.ChangingRadioPreset -= ChangingRadioPreset;
+            base.UnsubscribeEvents();
+        }
+
+        private void PickupAdded(PickupAddedEventArgs ev)
+        {
+            if (ev.Pickup is RadioPickup radio)
+            {
+                radio.IsEnabled = false;
+            }
+        }
+
+        private void OnTogglingRadio(TogglingRadioEventArgs ev)
+        {
+            if (!Check(ev.Player.CurrentItem)) return;
+            ev.IsAllowed = false;
+            
+        }
+
+        private void ChangingRadioPreset(ChangingRadioPresetEventArgs ev)
+        {
+            ev.IsAllowed = true;
+            ev.
+        }
     }
 }
